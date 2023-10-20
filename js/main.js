fe1332b72c8b99bb8d741a7e5a5a4e5d4e3ddce9
Ida Lampinen <lampinen89@gmail.com>
2023-10-20 20:42:06 +0200

    gjort klart uppgiften

--- js/main.js
+++ js/main.js
@@ -1,9 +1,48 @@
 let addContact = document.getElementById('add-contact');
 
+// funktion så knappen gör något när man klickar på den
 addContact.querySelector('button').addEventListener('click', function() {
-    let input = document.getElementsByTagName('fullname');
-    let newContact = document.createElement('div');
-    newContact.className = "added-to-contact-list";
-    addContact.innerText = input.value;
-    console.log('newContact');
-})
+    // En variabel som fångar alla inputs
+    let inputs = addContact.querySelectorAll('input');
+
+    // Variabel för den "osynliga" kontaktlistan
+    let contactList = document.getElementById('contact-list');
+    // variabel med det nya itemet
+    
+    const template = document.getElementById('contact-list-row');
+    
+    const listItem = template.content.cloneNode(true);
+    listItem.querySelector('input[type="text"]').value = inputs[0].value;
+    listItem.querySelector('input[type="tel"]').value = inputs[1].value;
+    
+    contactList.append(listItem);
+    
+    inputs[0].value = '';
+    inputs[1].value = '';
+});
+
+// Edit row
+function editRow(element){
+
+    let inputs = element.parentNode.querySelectorAll('input');
+    
+    if(element.classList.contains('save')){
+        inputs.forEach(input => {
+            input.disabled = true;
+        });
+        element.innerText = 'Ändra kontakt';
+        
+    } else {
+        inputs.forEach(input => {
+            input.disabled = false;
+        });
+        element.innerText = 'Spara ändringar';
+    }
+
+    element.classList.toggle("save");
+}
+
+// Delete row
+function deleteRow(element) {
+    element.parentNode.remove();
+}
